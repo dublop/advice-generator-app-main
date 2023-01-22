1ae581d53e4fb7ca7cd3783d9e93f6e829faba71
dublop <dnlopezavila@gmail.com>
2023-01-22 17:15:27 -0400

    Modify css file and html

--- assets/main.js
+++ assets/main.js
@@ -1,2 +1,26 @@
+const button = document.querySelector('#icon');
 const id = document.querySelector('#id');
-const text = document.querySelector('p');
+const advice = document.querySelector('#advice');
+const API = "https://api.adviceslip.com/advice";
+
+async function fetchData(API) {
+    const response = await fetch(API);
+    const data = await response.json();
+    return data;
+}
+
+const getData = async (API) => {
+    try {
+        const data = await fetchData(API);
+        id.textContent = data.slip.id;
+        advice.textContent = '"' + data.slip.advice + '"';
+    } catch(error) {
+        console.error(error);
+    }
+};
+
+button.onclick = () => {
+    getData(API);
+}
+
+getData(API);
